f0e9eb15a2c8573c19a39320f783d588c7a4fd6b
namcp <namchuphuong@admicro.vn>
2025-10-16 17:02:44 +0700

    done test v1

--- src/main/java/com/guno/dataimport/DataImportApplication.java
+++ src/main/java/com/guno/dataimport/DataImportApplication.java
@@ -2,11 +2,14 @@ package com.guno.dataimport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * Main Spring Boot Application
  */
 @SpringBootApplication
+@EnableScheduling
 public class DataImportApplication {
 
     public static void main(String[] args) {
